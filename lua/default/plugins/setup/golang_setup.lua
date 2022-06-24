c59caaa9b8370ce6bf53668a63451eec4af84bdf
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2022-06-24 13:57:17 +0700

    added ensurement

--- lua/default/plugins/setup/golang_setup.lua
+++ lua/default/plugins/setup/golang_setup.lua
@@ -2,7 +2,10 @@
 vim.api.nvim_create_autocmd('filetype', {
 	pattern = { 'go', 'gosum', 'gomod' },
 	callback = function()
-		require('default.plugins.lazy_setup.golang_setup')
+		-- ensures that the plugin is only loaded after golang plugin is done loaded.
+		vim.defer_fn(function()
+			require('default.plugins.lazy_setup.golang_setup')
+		end, 100)
 	end,
 	once = true,
 	desc = 'Call golang setup configs (oneshot)',
